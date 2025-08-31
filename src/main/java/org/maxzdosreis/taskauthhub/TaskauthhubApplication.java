03a177d67cb8f16424c98dccb23ecb702099b961
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-08-31 12:42:00 -0300

    chore: estruturação do projeto

---
+++ src/main/java/org/maxzdosreis/taskauthhub/TaskauthhubApplication.java
@@ -0,0 +1,13 @@
+package org.maxzdosreis.taskauthhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskauthhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskauthhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/maxzdosreis/taskauthhub/TaskauthhubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maxzdosreis.taskauthhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskauthhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
