f539e81e3c544604e61770bc28c5c2e9d489fb7e
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-08-31 22:20:11 -0300

    add(configs): adicionando algumas configs (OpenAPI, ContentNegotiation e Cors)

---
+++ src/main/java/org/maxzdosreis/bookauthorapi/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package org.maxzdosreis.bookauthorapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                    .title("Book Author API")
+                        .version("1.0")
+                        .description("API Documentation")
+                        .termsOfService("https://github.com/maxzdosreis")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")
+                        )
+                );
+    }
+}
